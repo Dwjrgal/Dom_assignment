f2404b0d8c2602855f55656e66c1e715f9795b46
dwjrgal <dwjrgl651@gmail.com>
2024-06-26 00:01:23 +0800

    js

--- Music_player/music.js
+++ Music_player/music.js
@@ -5,3 +5,54 @@ const nextBtn = document.querySelector('#next');
 const audio  = document.querySelector('#audio');
 // const musicContainer = document.querySelector('.player-box');
 // const musicContainer = document.querySelector('.player-box');
+
+
+//Song titles
+const songs = ['i need u','hey','summer']
+
+//Keep track of songs
+let songIndex =2
+
+//Initially load song info DOM
+loadSong(songs[songIndex])
+
+//Update song details
+function loadSong(song){
+    title.innerText =song
+    audio.src = `music/${song}.mp3`
+    cover.src = `images/${song}.jpg`
+}
+
+function playSong(){
+    player-box.classList.add('play')
+    playBtn.querySelector('i.fas').classList.remove('fa-play')
+    playBtn.querySelector('i.fas').classList.add('fa-pause')
+}
+
+function pauseSong(){
+    player-box.classList.remove('play')
+    playBtn.querySelector('i.fas').classList.add('fa-play')
+    playBtn.querySelector('i.fas').classList.remove('fa-pause')
+
+    audio.pause()
+}
+
+function prevSong(){
+    songIndex-
+
+}
+
+//Event listeners 
+playBtn.addEventListener('click',()=>{
+    const isPlaying = musicContainer.classList.contains('play')
+    if (isPlaying){
+        pauseSong()
+    }else{
+        playSong()
+    }
+
+})
+
+//Change song events
+prevBtn.addEventListener('click',prevSong)
+nextBtn.addEventListener('click',nextSong)
