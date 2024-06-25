95f44a6f2f21687b35331c4fc37bac6a898f1381
dwjrgal <dwjrgl651@gmail.com>
2024-06-25 23:11:16 +0800

    style

--- Music_player/music.js
+++ Music_player/music.js
@@ -1,2 +1,7 @@
-
-const audio
+const musicContainer = document.querySelector('.player-box');
+const playBtn  = document.querySelector('#play');
+const prevBtn = document.querySelector('#prev');
+const nextBtn = document.querySelector('#next');
+const audio  = document.querySelector('#audio');
+// const musicContainer = document.querySelector('.player-box');
+// const musicContainer = document.querySelector('.player-box');
