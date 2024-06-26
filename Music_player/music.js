947425a03ea71e895472fbb707189b521429069c
dwjrgal <dwjrgl651@gmail.com>
2024-06-26 19:46:00 +0800

    music

--- Music_player/music.js
+++ Music_player/music.js
@@ -1,79 +1,55 @@
-const musicContainer = document.querySelector('.player-box');
-const playBtn  = document.querySelector('#play');
-const prevBtn = document.querySelector('#prev');
-const nextBtn = document.querySelector('#next');
-const audio  = document.querySelector('#audio');
-// const musicContainer = document.querySelector('.player-box');
-// const musicContainer = document.querySelector('.player-box');
+const playBtn = document.getElementsByTagName("button")[0];
+const nextBtn = document.getElementsByTagName("button")[1];
+const audioPlayer = document.getElementsByTagName("audio")[0];
+const playIcon = document.getElementsByClassName("fa-play")[0];
+const plauseIcon = document.getElementsByClassName("fa-pause")[0];
+const songTitle = document.getElementById("songTitle");
 
+const playList = [
+  {
+    name: "UKULELE",
+    src: "./assets/songs/music-player_music_ukulele.mp3",
+  },
+  {
+    name: "SUMMER",
+    src: "./assets/songs/music-player_music_summer.mp3",
+  },
+];
+const playListName = ["SUMMER"];
+let currentSong = 0;
 
-//Song titles
-const songs = ['i need u','hey','summer']
-
-//Keep track of songs
-let songIndex =2
-
-//Initially load song info DOM
-loadSong(songs[songIndex])
-
-//Update song details
-function loadSong(song){
-    title.innerText =song
-    audio.src = `music/${song}.mp3`
-    cover.src = `images/${song}.jpg`
-}
-
-function playSong(){
-    player-box.classList.add('play')
-    playBtn.querySelector('i.fas').classList.remove('fa-play')
-    playBtn.querySelector('i.fas').classList.add('fa-pause')
-}
-
-function pauseSong(){
-    player-box.classList.remove('play')
-    playBtn.querySelector('i.fas').classList.add('fa-play')
-    playBtn.querySelector('i.fas').classList.remove('fa-pause')
-
-    audio.pause()
-}
-
-function prevSong(){
-    songIndex--
-
-    if (songIndex <0){
-        songIndex = songs.length -1
+playBtn.addEventListener("click", () => {
+  // duugaa togluulna
+  //   audioPlayer.paused // => true
+  //tuhain duu yavj bga eseh
+  console.log("CS", currentSong);
+  console.log("AP", audioPlayer.paused);
+  if (audioPlayer.paused) {
+    //tuhain duu zogsoj bga
+    audioPlayer.play();
+    // playBtn.textContent = "Pause";
+    playIcon.classList.add("hide");
+    plauseIcon.classList.remove("hide");
+    songTitle.textContent = playList[currentSong].name;
+  } else {
+    // tuhain duu yavj bga
+    audioPlayer.pause();
+     // playBtn.textContent = "Play";
+     playIcon.classList.remove("hide");
+     plauseIcon.classList.add("hide");
+   }
+   console.log("AP", audioPlayer.paused);
+ });
+ 
+nextBtn.addEventListener("click", () => {
+    currentSong++;
+    // daraagiin duug toguluulah
+    if (currentSong === playList.length) {
+      currentSong = 0;
     }
-
-    loadSong(songs[songIndex])
-
-    playSong()
-
-}
-
-function nextSong(){
-    songIndex++
-
-    if (songIndex > songs.lenght-1){
-        songIndex = 0
-    }
-
-    loadSong(songs[songIndex])
-
-    playSong()
-
-}
-
-//Event listeners 
-playBtn.addEventListener('click',()=>{
-    const isPlaying = musicContainer.classList.contains('play')
-    if (isPlaying){
-        pauseSong()
-    }else{
-        playSong()
-    }
-
-})
-
-//Change song events
-prevBtn.addEventListener('click',prevSong)
-nextBtn.addEventListener('click',nextSong)
+    audioPlayer.src = playList[currentSong].src;
+    songTitle.textContent = playList[currentSong].name;
+    audioPlayer.play();
+    console.log("CS", currentSong);
+  });
+  
